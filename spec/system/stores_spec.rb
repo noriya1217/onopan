1148877a8e5bb22fc6512a6b3ae308a1a9d67882
Noriya Saeki <noriya.saeki@gmail.com>
2019-08-11 15:51:09 +0900

    feat: 店舗プロフィール編集作成

--- spec/system/stores_spec.rb
+++ spec/system/stores_spec.rb
@@ -44,4 +44,50 @@ RSpec.describe 'Stores', type: :system do
       expect(page).to have_content 'store_indexメニュー画面'
     end
   end
+
+  describe 'プロフィール編集' do
+    it '編集失敗' do
+      visit root_url
+      fill_in 'store_email', with: 'test@nifty.com'
+      fill_in 'store_password', with: 'password'
+      click_button 'Log in'
+      visit store_path(Store.first.id)
+      click_on '編集'
+      fill_in 'store_name', with: ''
+      fill_in 'store_address', with: ''
+      fill_in 'store_email', with: ''
+      fill_in 'store_telephone', with: ''
+      fill_in 'store_access', with: ''
+      fill_in 'store_password', with: 'hogepiyo'
+      fill_in 'store_password_confirmation', with: 'hogepiyo'
+      fill_in 'store_current_password', with: 'password'
+      attach_file 'store_image', "#{Rails.root}/spec/factories/profile_image.jpg"
+      click_on 'Update'
+      expect(page).to have_content 'エラーが発生したため 店舗情報 は保存されませんでした'
+    end
+
+    it '編集成功' do
+      visit root_url
+      fill_in 'store_email', with: 'test@nifty.com'
+      fill_in 'store_password', with: 'password'
+      click_button 'Log in'
+      visit store_path(Store.first.id)
+      click_on '編集'
+      fill_in 'store_name', with: '築地パン屋さん'
+      fill_in 'store_address', with: '築地'
+      fill_in 'store_email', with: 'piyopiyo@yahoo.co.jp'
+      fill_in 'store_telephone', with: '090-1111-2222'
+      fill_in 'store_access', with: '築地駅から隅田川のほうへ6時間'
+      fill_in 'store_password', with: 'hogepiyo'
+      fill_in 'store_password_confirmation', with: 'hogepiyo'
+      fill_in 'store_current_password', with: 'password'
+      attach_file 'store_image', "#{Rails.root}/spec/factories/profile_image.jpg"
+      click_on 'Update'
+      visit store_path(Store.first.id)
+      expect(page).to have_content '築地パン屋さん'
+      expect(page).to have_content 'piyopiyo@yahoo.co.jp'
+      expect(page).to have_content '090-1111-2222'
+      expect(page).to have_content '築地駅から隅田川のほうへ6時間'
+    end
+  end
 end
