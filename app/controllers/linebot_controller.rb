e3fd4737fc767232209bac9545417ffc1e108be8
Noriya Saeki <noriya.saeki@gmail.com>
2019-08-15 22:23:13 +0900

    add: user削除処理追加

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -34,23 +34,31 @@ class LinebotController < ApplicationController
                 text: "この度はご注文いただきまして、誠にありがとうございます。\n大変申し訳ございませんが、売切となりました。\nまたのご利用をお待ちしております。"
               }
             end
+            client.reply_message(event['replyToken'], message)
           elsif event.message['text'].eql?('プロフィール')
-            message = {
-              type: 'text',
-              text: "プロフィール届いた"
-            }
+            client.reply_message(event['replyToken'], user_action_confirm)
           elsif event.message['text'].eql?('パン屋お気に入り')
             message = {
               type: 'text',
               text: "パン屋お気に入り届いた"
             }
+            client.reply_message(event['replyToken'], message)
           elsif event.message['text'].eql?("'onopanマニュアル")
             message = {
               type: 'text',
               text: "'onopanマニュアル届いた"
             }
+            client.reply_message(event['replyToken'], message)
+          elsif event.message['text'].eql?('プロフィール削除')
+            line_id = event['source']['userId']
+            user = User.find_by(line_id: line_id)
+            user.destroy
+            message = {
+              type: 'text',
+              text: 'プロフィール情報を削除しました。'
+            }
+            client.reply_message(event['replyToken'], message)
           end
-          client.reply_message(event['replyToken'], message)
         end
       end
     }
@@ -60,7 +68,38 @@ class LinebotController < ApplicationController
 
   private
 
-  def user_form
-
+  def user_action_confirm
+    {
+      "type": "template",
+      "altText": "This is a buttons template",
+      "template": {
+          "type": "buttons",
+          "text": "プロフィールに対しての操作を下記から選んで下さい",
+          "defaultAction": {
+              "type": "uri",
+              "label": "View detail",
+              "uri": "http://example.com/page/123"
+          },
+          "actions": [
+              {
+              "type": "uri",
+              "label": "作成",
+              "uri": "line://app/1599048461-KY1Q2QM8"
+              },
+              {
+                "type": "uri",
+                "label": "編集",
+                "uri": "http://example.com/page/123"
+                #collectionで作って、usercreate,そのlineIDとりだし、destroy、そして変数だけ使ってUser.find_by(line_id: line_id)。そんでedit
+              },
+              {
+                "type": "message",
+                "label": "削除",
+                "text": "プロフィール削除"
+                #削除するというメッセージを送って、またcollbackでうけとって、if文で処理して、削除する。
+              }
+          ]
+      }
+    }
   end
 end
