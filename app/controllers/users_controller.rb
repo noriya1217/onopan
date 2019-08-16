3f2b4530327814fb242f55ce5441c577ed454cc9
Noriya Saeki <noriya.saeki@gmail.com>
2019-08-16 19:49:35 +0900

    fix: user登録処理修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
     else
       user = User.last
       user_not_line_id = User.new(user_params)
-      if user.name == "nil" && user.email == "nil" && user.address == "nil" && user.line_id.present?
+      if user.email == "nil" && user.address == "nil" && user.line_id.present?
         user.name = user_not_line_id.name
         user.age = user_not_line_id.age
         user.sex = user_not_line_id.sex
