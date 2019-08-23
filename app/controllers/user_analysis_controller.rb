2ecdbe3b28fac5c88567eca0863015a34e3963ca
noriya <noriya.saeki@gmail.com>
2019-08-23 11:52:09 +0900

    fix: 顧客分析修正

--- app/controllers/user_analysis_controller.rb
+++ app/controllers/user_analysis_controller.rb
@@ -3,18 +3,18 @@ class UserAnalysisController < ApplicationController
 
   def index
     @store = Store.find(current_store.id)
-    gon.man = @store.users.where(sex: "男").count
-    gon.woman = @store.users.where(sex: "女").count
-    gon.teen = @store.users.where(age: 10).count
-    gon.twenties = @store.users.where(age: 20).count
-    gon.thirties = @store.users.where(age: 30).count
-    gon.forties = @store.users.where(age: 40).count
-    gon.fifties = @store.users.where(age: 50).count
-    gon.society = @store.users.where(profession: "社会人").count
-    gon.housewife = @store.users.where(profession: "主婦").count
-    gon.student = @store.users.where(profession: "学生").count
-    gon.part_time_job = @store.users.where(profession: "アルバイト").count
-    gon.unemployed = @store.users.where(profession: "無職").count
+    @man = gon.man = @store.users.where(sex: "男").count
+    @woman = gon.woman = @store.users.where(sex: "女").count
+    @teen = gon.teen = @store.users.where(age: 10).count
+    @twenties = gon.twenties = @store.users.where(age: 20).count
+    @thirties = gon.thirties = @store.users.where(age: 30).count
+    @forties = gon.forties = @store.users.where(age: 40).count
+    @fifties = gon.fifties = @store.users.where(age: 50).count
+    @society = gon.society = @store.users.where(profession: "社会人").count
+    @housewife = gon.housewife = @store.users.where(profession: "主婦").count
+    @student = gon.student = @store.users.where(profession: "学生").count
+    @part_time_job = gon.part_time_job = @store.users.where(profession: "アルバイト").count
+    @unemployed = gon.unemployed = @store.users.where(profession: "無職").count
   end
 end
 
